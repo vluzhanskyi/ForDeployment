b2301d6b352019f574c0254c5a9b94de6a2dac2c
vluzhanskyi <Vluzhanskyi@gmail.com>
2016-11-14 11:51:38 +0200

    Start

---
+++ Deplyment_TestSolution/Deplyment_TestSolution/Controller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deplyment_TestSolution
+{
+   public class Controller
+    {
+
+    }
+}
